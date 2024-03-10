8139b5388a6aab5e638f31ca7d8847623f9f5cc4
Pharnem <61577936+Pharnem@users.noreply.github.com>
2024-03-10 21:42:02 +0100

    feat: Added implementation for the SimpleReader class.

--- Storage/Reader/IReader.cs
+++ Storage/Reader/IReader.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Storage;
 public interface IReader
 {
+    public void Read();
     public DateTime Created();
     public TimeOnly Duration();
     public string Summary();
--- Storage/Reader/SimpleReader.cs
+++ Storage/Reader/SimpleReader.cs
@@ -9,14 +9,41 @@ public class SimpleReader : IReader
 {
     string _path;
     string _identity;
-
+    DateTime _created;
+    TimeOnly _duration;
+    string _summary;
+    string _fulltext;
     public SimpleReader(string path, string identity)
     {
         _path = path;
         _identity = identity;
+        _created = DateTime.MinValue;
+        _duration = new TimeOnly();
+        _summary = string.Empty;
+        _fulltext = string.Empty;
     }
-    public DateTime Created() => DateTime.MinValue;
-    public TimeOnly Duration() => TimeOnly.MaxValue;
-    public string Summary() => "I will be bothered to write an implementation on a later date.\n";
-    public string FullText() => "I will not be bothered to write an implementation on a later date.\n";
+
+    public void Read()
+    {
+        string contents;
+        using (StreamReader reader = new StreamReader(Path.Combine(_path,_identity)))
+        {
+            contents = reader.ReadToEnd();
+        }
+        contents.ReplaceLineEndings();
+        var parts = contents.Split(new string[] {"###CONFIG###","###SUMMARY###","###FULLTEXT###" }, StringSplitOptions.TrimEntries);
+        Dictionary<string,string> dict = new Dictionary<string,string>();
+        foreach (var assignment in parts[1].Split(Environment.NewLine)) {
+            var temp = assignment.Split('=', StringSplitOptions.TrimEntries);
+            dict[temp[0]] = temp[1];
+        }
+        _created = DateTime.Parse(dict["created"]);
+        _duration = TimeOnly.Parse(dict["duration"]);
+        _summary = parts[2];
+        _fulltext = parts[3];
+    }
+    public DateTime Created() => _created;
+    public TimeOnly Duration() => _duration;
+    public string Summary() => _summary;
+    public string FullText() => _fulltext;
 }
